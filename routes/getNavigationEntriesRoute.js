0a9726abae81ff201b68a1ddd5608d724adfca3a
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-11-17 18:25:07 +0100

    Created getNavigationEntries route
    - Created getNavigationEntries route for getting all navigation entries
    - The expected response in array of object, each object has title and slug and type

---
+++ routes/getNavigationEntriesRoute.js
@@ -0,0 +1,33 @@
+const ContentfulClient = require("../utils/contentful");
+
+module.exports = async (req, res) => {
+  const contentfulClientInstance = new ContentfulClient();
+  const client = contentfulClientInstance.getClient();
+
+  client
+    .getEntries({
+      content_type: "navigationEntry",
+    })
+    .then((response) => {
+      let modifiedResponse = [];
+      response.items.map((item) => {
+        console.log(item.fields.link);
+        let title = item.fields.link.fields.title;
+        let slug = item.fields.link.fields.slug;
+        let tag = item.fields.link.fields.tag;
+        let category = item.fields.link.fields.category;
+        const type =
+          category != undefined
+            ? "category"
+            : tag != undefined
+            ? "tag"
+            : "none";
+        modifiedResponse.push({ title, slug, type });
+      });
+      res.send(modifiedResponse);
+    })
+    .catch((error) => {
+      console.log("Error occurred while fetching entries:", error);
+      res.status(400).send(error);
+    });
+};
