bdfd115663073cb1eff7bbaf85d53de157c1686c
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-11-28 11:58:13 +0100

    Modified returned JSON

--- routes/getProductsByCategoryRoute.js
+++ routes/getProductsByCategoryRoute.js
@@ -9,9 +9,8 @@ module.exports = async (req, res) => {
     const categoryId = req.query.categoryId;
     const categoryName = await getCategoryName(categoryId ?? undefined);
     const allProducts = await getAllProducts();
-    console.log(allProducts.body.results);
     const filteredProducts = await filterProducts(categoryId, allProducts);
-    res.status(200).send({ categoryName, filteredProducts });
+    res.status(200).send({ categoryName, products: filteredProducts });
   } catch (error) {
     console.log(`Error while fetching products by category! ${error}`);
     res.status(400).send(error);
