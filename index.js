23b6544c43ab3c446a04a123cd2b616f5bc8ea13
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-12-11 17:44:58 +0100

    Add API for creating cart ID
    -added /createCart API that created cart and return the id of the cart

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const getHomePageRoute = require("./routes/getHomePageRoute");
 const getProductRoute = require("./routes/getProductRoute");
 const getProductsByCategory = require("./routes/getProductsByCategoryRoute");
 const getProductsByCategoriesRoute = require("./routes/getProductsByCategoriesRoute");
+const createCartRoute = require("./routes/createCartRoute");
 
 dotenv.config();
 
@@ -27,9 +28,12 @@ app.use(express.json());
 app.get("/getNavigationEntries", getNavigationEntriesRoute);
 app.get("/getHomePage", getHomePageRoute);
 app.get("/getPage", getPageRoute);
+
 app.get("/getProductsByCategory", getProductsByCategory);
 app.get("/getProductsByCategories", getProductsByCategoriesRoute);
 app.get("/getProduct", getProductRoute);
+
+app.post("/createCart", createCartRoute);
 
 app.listen(port, () => {
   console.log(`Product service on port: ${port}`);
