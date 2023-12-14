4e84acdc8c2acc837749bb9232e39c3501c55b84
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-12-14 18:37:11 +0100

    Added null return for customer cart
    If customer does not have a cart, return null as cartId

--- routes/getCartByCustomerIdRoute.js
+++ routes/getCartByCustomerIdRoute.js
@@ -9,9 +9,10 @@ module.exports = async (req, res) => {
       uri: `/airexpress/carts/customer-id=${customerId}`,
     });
 
-    res.status(200).send({ cartId: createdCart.body.id ?? null });
+    res.status(200).send({ cartId: createdCart.body.id });
   } catch (error) {
     console.log(`Error while creating cart! ${error}`);
-    res.status(503).send({ error: error });
+    if (error.code == 404) res.status(200).send({ cartId: null });
+    else res.status(503).send({ error: error });
   }
 };
