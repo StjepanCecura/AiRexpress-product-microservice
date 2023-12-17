58a8ca678eba0920adb1734d29fade54277fc3e5
mfric20 <100581678+mfric20@users.noreply.github.com>
2023-12-17 19:38:51 +0100

    Added API for creating order
    - Added API post request for creating order on path /createOrder

--- routes/createOrderRoute.js
+++ routes/createOrderRoute.js
@@ -1,5 +1,23 @@
+const { commercetoolsClient } = require("../utils/commercetools.js");
+
 module.exports = async (req, res) => {
   try {
+    const cartId = req.body.cartId;
+    const version = req.body.version;
+
+    const order = await commercetoolsClient.execute({
+      method: "POST",
+      uri: `/airexpress/orders`,
+      body: {
+        cart: {
+          id: cartId,
+          typeId: "cart",
+        },
+        version,
+      },
+    });
+
+    res.status(200).send(order);
   } catch (error) {
     console.log("Error creating order!");
     res.status(503).send({ error });
